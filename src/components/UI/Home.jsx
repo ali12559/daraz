59fa55f47b2bd8040a8e9e92c9e97844b7d052f0
Ali Raza <ar2062002@gmail.com>
2026-01-19 23:33:32 +0500

    update

--- src/components/UI/Home.jsx
+++ src/components/UI/Home.jsx
@@ -2,36 +2,62 @@ import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
 
 function Home() {
-  let [products , setProducts] = useState([]);
+  let [products, setProducts] = useState([]);
+  const [category, setCategory] = useState([]);
 
-   const fetchProducts = async () => {
-    try{
-      const response = await fetch("https://fakestoreapi.com/products")
+  const fetchProducts = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
       if (!response.ok) {
         console.log("Failed to fetch products.");
       }
       const data = await response.json();
-      setProducts(data)
+      setProducts(data);
     } catch (err) {
-      console.log(err)
+      console.log(err);
     }
-  }
-  
+  };
+
   useEffect(() => {
-    fetchProducts()
-  }, [])
+    fetchProducts();
+  }, []);
+
+  const toggleCategory = (e) => {
+    const value = e.target.value;
+
+    setCategory((prev) =>
+      prev.includes(value)
+        ? prev.filter((item) => item !== value)
+        : [...prev, value],
+    );
+  };
+
+  const filteredProducts =
+    category.length === 0
+      ? products
+      : products.filter((product) => category.includes(product.category));
 
   return (
     <div className="home text-center">
       <h2 className="text-[#424242] text-[22px] my-2 ">Flash Sale</h2>
 
-      <div className="grid grid-cols-5 items-center">
-        {products.map((product) => 
-          <ProductCard key={product.id} product={product} />
-        )}
+      <div>
+        <p>
+          <input type="checkbox" value={"men's clothing"} onClick={toggleCategory} /> men's clothing
+        </p>
+        <p> 
+          <input type="checkbox" value={"women's clothing"} onClick={toggleCategory} /> women's clothing
+        </p>
+        <p>
+          <input type="checkbox" value={"jewelery"} onClick={toggleCategory} /> jewelery
+        </p>
       </div>
 
-      
+      <div className="grid grid-cols-5 items-center">
+        {filteredProducts.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
     </div>
   );
 }
--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -2,9 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export const ProductDetails = () => {
-  let [products, setProducts] = useState({});
-  let { id } = useParams;
-  // console.log(useParams())
+    let [products, setProducts] = useState({});
+    let { id } = useParams();
+    // console.log(useParams())
 
   const fetchProduct = async () => {
     try {
@@ -25,14 +25,15 @@ export const ProductDetails = () => {
 
   return (
     <>
-    <div>
-      <h1 className="bg-gray-400 w-fit px-2 py-0.5 rounded-4xl text-gray-50">
+    <div className="m-5">
+      <h1 className="bg-gray-400 w-fit px-2 py-0.5 m-5 rounded-4xl text-gray-50">
         {products.category}
       </h1>
       <img src={products.image} alt={products.title} />
       <h1>{products.title}</h1>
       <p>{products.description}</p>
       <p>${products.price}</p>
+
     </div>
     </>
   );
